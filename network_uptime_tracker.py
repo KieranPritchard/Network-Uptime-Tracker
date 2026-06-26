b6478ab915e2d16b4c5f4668e44caebe47ac7d48
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-06-26 16:36:06 +0100

    created get_all_server_locations function

--- network_uptime_tracker.py
+++ network_uptime_tracker.py
@@ -1,4 +1,7 @@
 import streamlit as st
 
+def get_all_server_locations(filepath: str) -> list:
+    """Function to get all of the server locations from a file"""
+
 # Sets the page configuration
 st.set_page_config(page_title="Server Status Dashboard", layout="wide")
