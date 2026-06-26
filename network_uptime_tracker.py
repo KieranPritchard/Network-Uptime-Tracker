8e30eebefc8d60d7995174989439ac4220650c77
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-06-26 17:14:22 +0100

    added some more cleaning

--- network_uptime_tracker.py
+++ network_uptime_tracker.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import socket
 import os
+import re
+
+# Stores the ipv4 pattern
+IPV4_PATTERN = re.compile(
+    r'^((25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)\.){3}'
+    r'(25[0-5]|2[0-4]\d|1\d{2}|[1-9]\d|\d)$'
+)
 
 def get_all_server_locations(filepath: str) -> list:
     """Function to get all of the server locations from a file"""
@@ -24,8 +32,32 @@ def get_all_server_locations(filepath: str) -> list:
         # Reads the file and saves the location to a variable
         locations = f.readlines()
 
+    # Stores the resolved locations
+    resolved = []
+
+    # Loops over each of the locations
+    for location in locations:
+        # Skips a blank line
+        if not location:
+            continue
+
+        # Checks if the location is already a valid location
+        if IPV4_PATTERN.match(location):
+            # Adds the pattern to the reolved
+            resolved.append(location)
+        else:
+            # Treat it as a hostname and attempt to resolve it to an IP
+            try:
+                # gets the ip address from the host name
+                ip = socket.gethostbyname(location)
+                # Adds it to the resolved page
+                resolved.append(ip)
+            except socket.gaierror:
+                # Displays a warning message
+                st.warning(f"Could not resolve '{location}' — skipping")
+
     # Returns the locations variable
-    return locations
+    return resolved
 
 # Sets the page configuration
 st.set_page_config(page_title="Server Status Dashboard", layout="wide")
