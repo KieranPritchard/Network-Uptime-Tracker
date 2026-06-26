56c0b890281416dcfc9b9f75aec6f1d9945afab2
Kieran Pritchard <KieranPritchard06@gmail.com>
2026-06-26 16:53:28 +0100

    added code bring in the server information

--- network_uptime_tracker.py
+++ network_uptime_tracker.py
@@ -1,7 +1,31 @@
 import streamlit as st
+import os
 
 def get_all_server_locations(filepath: str) -> list:
     """Function to get all of the server locations from a file"""
+
+    # Checks if the filepath was passed into the function
+    if not filepath:
+        # Outputs an error to the screen
+        st.error("A file path must be specified")
+
+    # Checks if the file path exists
+    if not os.path.exists(filepath):
+        # Outputs an error to the screen
+        st.error("A file path does not exist")
+
+    # Checks if the file is a txt file
+    if not filepath.endswith(".txt"):
+        # Outputs an error to the screen
+        st.error("The file must be a .txt")
+
+    # Opens the file
+    with open(filepath) as f:
+        # Reads the file and saves the location to a variable
+        locations = f.readlines()
+
+    # Returns the locations variable
+    return locations
 
 # Sets the page configuration
 st.set_page_config(page_title="Server Status Dashboard", layout="wide")
